5ba14acf5bdcb7c405f7b2874952e1f052f6683a
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-06-02 09:24:00 +0800

    s

--- 1-fv/getVideoDarwin.m
+++ 1-fv/getVideoDarwin.m
@@ -16,7 +16,6 @@ function getVideoDarwin(fullvideoname,featDir_FV,descriptor_path,gmmSize,mbhFeat
             if exist(mbhfeatFile,'file') == 2
                 timest = tic();
                 data = dlmread(mbhfeatFile);
-                data = reshape(data,dimension, size(data,2)/dimension)';
                 w = VideoDarwin(data);
                 dlmwrite(wFile,w');
                 clear data;

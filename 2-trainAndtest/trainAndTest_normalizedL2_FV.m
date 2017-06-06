a47187a86c20b2b0d631b2f469899a87e662f433
chengy35 <chengy35@mail2.sysu.edu.cn>
2017-06-06 15:25:13 +0800

    s

--- 2-trainAndtest/trainAndTest_normalizedL2_FV.m
+++ 2-trainAndtest/trainAndTest_normalizedL2_FV.m
@@ -42,7 +42,7 @@ function trainAndTest_normalizedL2_FV(video_data_dir,fullvideoname,featDir_FV,fe
 				featFile{j} = fullfile(featDir_FV,'wmbh',sprintf('%s.mat',partfile));  
 				fprintf('read wmbh in testing : %d \n',j);
 				temp = dlmread(featFile{j});
-				TestData(j,:)  = normalize(sqrt(temp'),'Power-Intra-L2',2*256);
+				TestData(j,:)  = normalize(sqrt(temp'),'Power-Intra-L2',4*256);
 				clear temp;
 				
 			end
